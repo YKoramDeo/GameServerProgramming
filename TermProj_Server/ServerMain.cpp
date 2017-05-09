8d190a7fc9218fab0d6e4afac9337548677b5be0
ykoramdeo <ykoramdeo@gmail.com>
2017-05-10 01:11:29 +0900

    server / Accept Thread 상세 동작 내용 추가 구현 ADD:: 가 덧붙인 주석은 해당 내용은 구현이 필요한 부분이라는 표시

--- TermProj_Server/ServerMain.cpp
+++ TermProj_Server/ServerMain.cpp
@@ -7,10 +7,10 @@ void DisplayDebugText(std::string);
 void DisplayErrMsg(char*, int);
 
 void InitializeServerData(void);
-void StopServer();
-void AcceptThreadFunc();
+void StopServer(void);
+void AcceptThreadFunc(void);
 
-int main()
+int main(int argc, char *argv[])
 {
 	// 01. Main문은 다음과 같이 동작하도록 한다.
 	InitializeServerData();
@@ -25,10 +25,10 @@ int main()
 	for (int i = 0; i < NUM_THREADS; ++i)
 		worker_threads.push_back(new std::thread(WorkerThreadFunc));
 	
-	for (auto th : worker_threads)
+	for (auto thread : worker_threads)
 	{
-		th->join();
-		delete th;
+		thread->join();
+		delete thread;
 	}
 
 	accept_thread.join();
@@ -36,21 +36,21 @@ int main()
 	return 0;
 }
 
-void DisplayDebugText(std::string str)
+void DisplayDebugText(std::string msg)
 {
-	std::cout << str << std::endl;
+	std::cout << msg << std::endl;
 	return;
 }
 
-void DisplayErrMsg(char* str, int errNo)
+void DisplayErrMsg(char* str, int err_no)
 {
-	WCHAR *lpMsgBuf;
+	WCHAR *msg_buf;
 	FormatMessage(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, errNo, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	printf("%s", str); wprintf(L" %d Error :: %s\n", errNo, lpMsgBuf);
-	LocalFree(lpMsgBuf);
+		NULL, err_no, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&msg_buf, 0, NULL);
+	printf("%s", str); wprintf(L" %d Error :: %s\n", err_no, msg_buf);
+	LocalFree(msg_buf);
 	// FormatMessage() : 오류코드에 대응하는 오류 메시지를 얻을 수 있다.
 
 	// FORMAT_MESSAGE_ALLOCATE_BUFFER			: 오류 메시지를 저장할 공간을 함수가 알아서 할당한다는 의미
@@ -74,7 +74,6 @@ void InitializeServerData(void)
 		//			: 프로그램을 종료하기 전에 버퍼를 모두 삭제하고 열린 파일을 모두 닫음. 
 		//			: EXIT_SUCCESS 또는 0은 정상종료, 그렇지 않으면 다른 상태 값이 리턴.
 	}
-	
 	// WSAStartup()	: 윈속 초기화 함수로, 프로그램에서 사용할 윈속 버전을 요청함으로써 윈속 라이브러리(WS2_32.DLL)를 초기화 하는 역할.
 	//				: return - 성공하면 0, 실패하면 오류코드
 
@@ -89,29 +88,123 @@ void InitializeServerData(void)
 		DisplayDebugText("ERROR :: InitializeServerData :: [ CreateIoCompletionPort ] Fail !!");
 		exit(EXIT_FAILURE);
 	}
-	
 	// CreateIoCompletionPort()	: 1. 입출력 완료 포트를 생성.
 	//							: 2. 소켓과 입출력 완료포트를 연결.
 	//								소켓과 입출력 완료포트를 연결해두면 이 소켓에 대한 비동기 입출력 결과가 입출력 완료포트에 저장.
+	
 	DisplayDebugText("Initialize Server Data Success!");
 	return;
 }
 
-void StopServer()
+void StopServer(void)
 {
 	if (WSACleanup() != 0) {
 		DisplayErrMsg("Error :: StopServer :: [ WSACleanup ] Fail !!", GetLastError());
 		exit(EXIT_FAILURE);
 	}
-
 	// WSACleanup() : 프로그램 종료 시 윈속 종료 함수
 	//				: 윈속 사용을 중지함을 운영체제에 알리고, 관련 리소르스를 반환.
 
 	return;
 }
 
-void AcceptThreadFunc()
+void AcceptThreadFunc(void)
 {
-	DisplayDebugText("AcceptThreadFunc :: Run!!");
+	SOCKET accept_sock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (INVALID_SOCKET == accept_sock)
+	{
+		DisplayErrMsg("Error :: AcceptThreadFunc :: Create Accept Socket Fail !!", WSAGetLastError());
+		return;
+	}
+	// WSASocket(1, 2, 3, 4, 5, 6) : socket을 생성하는 함수
+	// 1. af			 : address family - AF_INET만 사용 (그 밖에 AF_NETBIOS, AF_IRDA, AF_INET6가 존재.)
+	// 2. type			 : 소켓의 타입 - tcp를 위해 SOCKET_STREAM사용 (SOCK_DGRAM : udp)
+	// 3. protocol		 : 사용할 프로토콜의 종류 - IPPROTO_TCP (IPPROTO_UDP)
+	// 4. lpPtotocolInfo : 프로토콜 정보 - 보통 NULL
+	// 5. g				 : 예약
+	// 6. dgFlags		 : 소켓의 속성 - 보통 0 (또는 WSA_PROTOCOL_OVERLAPPED)
+
+	struct sockaddr_in listen_addr;
+	ZeroMemory(&listen_addr, sizeof(listen_addr));
+	listen_addr.sin_family = AF_INET;
+	listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	listen_addr.sin_port = htons(MY_SERVER_PORT);
+	ZeroMemory(&listen_addr.sin_zero, 8);
+	// sockaddr_in : 소켓 주소 구조체 socket address structures
+	//			   : 네트워크 프로그램에서 필요한 주소 정보를 담고 있는 구조체.
+	//			   : 특히, 여기서 사용된 sockaddr_in은 IPv4에 사용된 소켓 주소 구조체.
+	// 1. short		sin_family	: 주소 체계를 의미하며 AF_INET(인터넷 주소 체계, IPv4) 값을 사용한다.
+	// 2. struct in_addr sin_addr 
+	//			: IP 주소를 의미하며, 각각 32비트 in_addr 구조체를 사용한다.
+	//			: 응용프로그램에서는 대개 32비트 단위로 접근하므로 S_un.S_addr필드를 사용하고 매크로를 통해 재정의 된 s_addr을 사용하면 편하다.
+	//			: INADDR_ANY	: 서버의 지역 IP주소를 설정하는데 있어 다음과 같이 설정하면 서버가 IP주소를 2개 이상 보유할 경우,
+	//						    : (multihomed host 라고 부름), 클라이언트가 어느 IP주소로 접속하든 받아들일 수 있도록 설정.
+	// 3. u_short	sin_port	: 포트 번호를 의미, 부호 없는 16비트 정수 값을 사용한다.
+	// 4. char		sin_zero[8] : 0으로 설정하면 되는 여유 공간이다.
+
+	int ret_val = 0;
+	ret_val = ::bind(accept_sock, reinterpret_cast<sockaddr*>(&listen_addr), sizeof(listen_addr));
+	if (SOCKET_ERROR == ret_val)
+	{
+		DisplayErrMsg("Error :: AcceptThreadFunc :: Bind Fail !!", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	// bind() : 소켓의 지역 IP주소와 지역 포트 번호를 결정, 
+	//		  : C++11의 bind라는 함수와 혼동 때문에 :: 범위확인 연산자와 같이 사용
+	// listen_addr : 지역 IP와 지역 포트 번호로 초기화한 소켓 주소 구조체를 전달.
+	// bind 함수의 2번째 인자는 항상 (SOCKADDR*)형을 반환해야 함.
+
+	ret_val = listen(accept_sock, SOMAXCONN);
+	if (SOCKET_ERROR == ret_val)
+	{
+		DisplayErrMsg("Error :: AcceptThreadFunc :: Listen Fail !!",WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	// listen()  : 소켓의 TCP 포트 상태를 LISTENING상태로 바꾼다. 이는 클라이언트 접속을 받아들일 수 있는 상태가 됨을 의미
+	// SOMAXCONN : 클라이언트의 접속 정보는 연결 큐에 저장되는데, backlog는 이 연결 큐의 길이를 나타냄.
+	//			 : SOMAXCONN의 의미는 하부 프로토콜에서 지원 가능한 최댓값을 사용
+
+	while (true)
+	{
+		int new_id = -1; // 접속한 클라이언트의 새 ID, 먼저 사용하지 않는 -1로 초기화
+		struct sockaddr_in client_addr;
+		int addr_size = sizeof(client_addr);
+
+		SOCKET new_client_sock = WSAAccept(accept_sock, reinterpret_cast<SOCKADDR*>(&client_addr), &addr_size, NULL, NULL);
+		if (INVALID_SOCKET == new_client_sock)
+		{
+			DisplayErrMsg("Error :: AcceptThreadFunc :: WSAAccept Fail !!", WSAGetLastError());
+			break;
+		}
+		// WSAAccept()	: 접속한 클라이언트와 통신할 수 있도록 새로운 소켓을 생성해서 리턴 또한 접속한 클라이언트의 주소정보도 알려줌
+		//				: 서버입장에서는 원격 IP주소와 원격 포트번호, 클라이언트 입장에서는 지역 IP주소와 지역 포트번호
+		
+		// ADD::접속한 클라이언트의 새로운 ID를 부여하는 구간
+		if (-1 == new_id)
+		{
+			DisplayDebugText("AcceptThread :: Maximum User Number Sorry :(");
+			closesocket(new_client_sock);
+			continue;
+		}
+
+		// Accept 받은 이후에 등록을 해주어야 함.
+		HANDLE result = CreateIoCompletionPort(reinterpret_cast<HANDLE>(new_client_sock), ghIOCP, new_id, 0);
+		if (NULL == result)
+		{
+			DisplayErrMsg("Error :: AcceptThreadFunc :: CreateIoCompletionPort Fail !!", WSAGetLastError());
+			closesocket(new_client_sock);
+			continue;
+		}
+		// CreateIoCompletionPort() : 소켓과 입출력 완료포트를 연결.
+		//							: 소켓과 입출력 완료포트를 연결해두면 이 소켓에 대한 비동기 입출력 결과가 입출력 완료포트에 저장.
+
+		// ADD::연결된 클라이언트 Network 정보 초기화
+		// ADD::연결된 새로운 소켓 Recv수행.
+	}
+	return;
+}
+
+void WorkerThreadFunc(void)
+{
 	return;
 }
